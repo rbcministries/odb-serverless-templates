9143ccbc22d6d89071d8c24c46cf7b524e660a13
BJ Clark <bjclark13@outlook.com>
2021-04-13 08:51:53 -0400

    Update for latest best practices

--- odb-typescript-api/handler.ts
+++ odb-typescript-api/handler.ts
@@ -3,69 +3,88 @@
  * request. Please modify as needed.
  */
 
-import { APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
-import 'source-map-support/register';
-import * as HttpStatus from 'http-status-codes';
+import {
+	APIGatewayEvent,
+	APIGatewayProxyHandler,
+	APIGatewayProxyResult,
+	Context,
+} from 'aws-lambda';
+import { StatusCodes } from 'http-status-codes';
 
 // Abstract business logic into API
 import API from './Api';
 
 const api = new API();
 
-const lambdaProxyResponse = (data: {}, statusCode: number = HttpStatus.OK) : APIGatewayProxyResult => {
-  return {
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    statusCode: statusCode,
-    body: JSON.stringify(data), // Consider obsfucating the error if it poses a security risk
-  }
+const lambdaProxyResponse = (
+	data: {},
+	statusCode: number = StatusCodes.OK
+): APIGatewayProxyResult => {
+	return {
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+		},
+		statusCode: statusCode,
+		body: JSON.stringify(data), // Consider obsfucating the error if it poses a security risk
+	};
 };
 
-export const getMethod: APIGatewayProxyHandler = async (event, _context) => {
-  // Access params from event
-  const params = event.queryStringParameters ? event.queryStringParameters : {};
+export const getMethod: APIGatewayProxyHandler = async (
+	event: APIGatewayEvent,
+	_context: Context
+) => {
+	// Access params from event
+	const params = event.queryStringParameters ? event.queryStringParameters : {};
 
-  try {
-    const data = await api.get(params);
+	try {
+		const data = await api.get(params);
 
-    return lambdaProxyResponse(data);
-  } catch (err) {
-    return lambdaProxyResponse(err, HttpStatus.INTERNAL_SERVER_ERROR);
-  }
-}
+		return lambdaProxyResponse(data);
+	} catch (err) {
+		return lambdaProxyResponse(err, StatusCodes.INTERNAL_SERVER_ERROR);
+	}
+};
 
-export const postMethod: APIGatewayProxyHandler = async (event, _context) => {
-  try {
-    const body = JSON.parse(event.body);
-    const data = await api.post(body);
-    
-    return lambdaProxyResponse(data, HttpStatus.CREATED);
-  } catch (err) {
-    return lambdaProxyResponse(err, HttpStatus.INTERNAL_SERVER_ERROR);
-  }
-}
+export const postMethod: APIGatewayProxyHandler = async (
+	event: APIGatewayEvent,
+	_context: Context
+) => {
+	try {
+		const body = JSON.parse(event.body);
+		const data = await api.post(body);
 
-export const putMethod: APIGatewayProxyHandler = async (event, _context) => {
-  try {
-    const body = JSON.parse(event.body);
-    const id = event.pathParameters.id;
+		return lambdaProxyResponse(data, StatusCodes.CREATED);
+	} catch (err) {
+		return lambdaProxyResponse(err, StatusCodes.INTERNAL_SERVER_ERROR);
+	}
+};
 
-    const data = await api.put(id, body);
-    
-    return lambdaProxyResponse(data);
-  } catch (err) {
-    return lambdaProxyResponse(err, HttpStatus.INTERNAL_SERVER_ERROR);
-  }
-}
+export const putMethod: APIGatewayProxyHandler = async (
+	event: APIGatewayEvent,
+	_context
+) => {
+	try {
+		const body = JSON.parse(event.body);
+		const id = event.pathParameters.id;
 
-export const deleteMethod: APIGatewayProxyHandler = async (event, _context) => {
-  try {
-    const id = event.pathParameters.id;
-    const data = await api.delete(id);
-    
-    return lambdaProxyResponse(data);
-  } catch (err) {
-    return lambdaProxyResponse(err, 500);
-  }
-}
+		const data = await api.put(id, body);
+
+		return lambdaProxyResponse(data);
+	} catch (err) {
+		return lambdaProxyResponse(err, StatusCodes.INTERNAL_SERVER_ERROR);
+	}
+};
+
+export const deleteMethod: APIGatewayProxyHandler = async (
+	event: APIGatewayEvent,
+	_context: Context
+) => {
+	try {
+		const id = event.pathParameters.id;
+		const data = await api.delete(id);
+
+		return lambdaProxyResponse(data);
+	} catch (err) {
+		return lambdaProxyResponse(err, 500);
+	}
+};
